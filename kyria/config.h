3d2c018139f0787efaf3d61709dfb051cb48eb80
Patrick Elmquist <patrick.j.elmquist@gmail.com>
2021-09-09 20:46:33 +0200

    Updated Kyria keymap and migrated to OLED_ENABLE

--- kyria/config.h
+++ kyria/config.h
@@ -21,7 +21,7 @@
 
 #define SPLIT_MODS_ENABLE
 
-#ifdef OLED_DRIVER_ENABLE
+#ifdef OLED_ENABLE
   #define OLED_DISPLAY_128X64
 #endif
 
--- user/rules.mk
+++ user/rules.mk
@@ -15,7 +15,7 @@ UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight. 
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_DRIVER_ENABLE= yes     # OLED display
+OLED_ENABLE = yes     # OLED display
 
 AUTO_SHIFT_ENABLE = no
 WPM_ENABLE = no
